dafd1faeff25c45688a251030f8d55f295ec33e7
SEUNGHEE LEE <lee2002w@gmail.com>
2022-07-25 14:22:55 +0900

    check dir exist

---
+++ posix_check_dir_exist/src/main.cpp
@@ -0,0 +1,34 @@
+#include <sys/stat.h>
+#include <string>
+#include <iostream>
+
+bool check_dir_exist(const char *path) {
+    bool exist = false;
+    struct stat st{};
+    if(stat(path, &st) == 0) {
+        if( ((st.st_mode) & S_IFMT) == S_IFDIR ) {
+            exist = true;
+        }
+    }
+
+    return exist;
+}
+
+int main(int argc, char *argv[]) {
+    std::string path1("./test_dir_1");
+    std::string path2("./test_dir_2");
+
+    if (check_dir_exist(path1.c_str())) {
+        std::cout << "Path1 (" << path1 << ") Exist\n";
+    } else {
+        std::cout << "Path1 (" << path1 << ") Not Exist\n";
+    }
+
+    if (check_dir_exist(path2.c_str())) {
+        std::cout << "Path2 (" << path2 << ") Exist\n";
+    } else {
+        std::cout << "Path2 (" << path2 << ") Not Exist\n";
+    }
+
+    return 0;
+}
